c58bf42e7907f7177e4369ae7b00881197194755
Bryan Bosire <bryanbosire@gmail.com>
2022-09-07 11:32:19 +0300

    Added crud methods in the controller + tested end points using POSTMAN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,42 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # GET /musics:
+  get '/musics' do
+    musics = Music.all.order(:created_at)
+    musics.to_json
+  end
+
+  # POST /musics:
+  post '/musics' do
+    music = Music.create(
+      musicName: params[:musicName],
+      artist: params[:artist],
+      Ratings: params[:Ratings],
+      link: params[:link],
+      lyrics: params[:lyrics]
+    )
+    music.to_json
+  end
+
+  # PATCH /musics/:id: updates
+  patch '/musics/:id' do
+    music = Music.find(params[:id])
+    music.update(
+      musicName: params[:musicName],
+      artist: params[:artist],
+      Ratings: params[:Ratings],
+      link: params[:link],
+      lyrics: params[:lyrics]
+    )
+    music.to_json
+  end
+
+  # DELETE /musics/:id: deletes the music from the database.
+  delete '/musics/:id' do
+    music = Music.find(params[:id])
+    music.destroy
+    music.to_json
   end
 
 end
